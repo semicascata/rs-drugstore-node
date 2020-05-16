ff8315da922de851c43f6e0995d0570dd2924377
r. drt <51922521+semicascata@users.noreply.github.com>
2020-05-16 12:07:06 -0300

    Refresh token OK

--- src/controllers/AuthControl.js
+++ src/controllers/AuthControl.js
@@ -1,38 +1,44 @@
 import User from '../models/UserSchema'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
-let tokenList = []
+let tokenList = {}
 
 export default {
   // @desc Register handler
   // @route POST /shelter/v1/auth/register
   // @access Public
-  async register (req, res) {
-    const { username, email, password, password2, role } = req.body
+  async register(req, res) {
+    const {
+      username,
+      email,
+      password,
+      password2,
+      role
+    } = req.body
     let errors = [] // Validation errors
 
     // Check empty fields
-    if(!username || !email || !password || !password2) {
+    if (!username || !email || !password || !password2) {
       errors.push({
         message: 'Please, fill in all fields'
       })
     }
 
     // Password match
-    if(password !== password2) {
+    if (password !== password2) {
       errors.push({
         message: 'Passwords don\'t match'
       })
     }
 
     // Password length
-    if(password.length < 6) {
+    if (password.length < 6) {
       errors.push({
         message: 'Password should be at least 6 charaters'
       })
     }
 
-    if(errors.length > 0) {
+    if (errors.length > 0) {
       // If there is any validation errors
       console.log(errors);
       res.status(401).send(
@@ -45,58 +51,63 @@ export default {
 
     } else {
       // Check if user exists
-      await User.findOne({ username })
-      .then(user => {
-        if(user) {
-          console.log(`\nUser ${username} already exists`.red.bold)
-          errors.push({
-            message: `User ${username} already exists`
-          })
+      await User.findOne({
+          username
+        })
+        .then(user => {
+          if (user) {
+            console.log(`\nUser ${username} already exists`.red.bold)
+            errors.push({
+              message: `User ${username} already exists`
+            })
 
-          res.status(401).send(
-            errors,
-            username,
-            email,
-            password,
-            password2
-          )
+            res.status(401).send(
+              errors,
+              username,
+              email,
+              password,
+              password2
+            )
 
-        } else {
-          // If its a new user
-          const newUser = new User({
-            username,
-            email,
-            password,
-            role
-          })
+          } else {
+            // If its a new user
+            const newUser = new User({
+              username,
+              email,
+              password,
+              role
+            })
 
-          // Save user on DB
-          newUser.save()
-          .then(user => {
+            // Save user on DB
+            newUser.save()
+              .then(user => {
 
-            console.log(`User "${newUser.username}" registered\n User role: ${newUser.role}`.green.bold)
+                console.log(`User "${newUser.username}" registered\n User role: ${newUser.role}`.green.bold)
 
-            res.status(201).send(user)
-          })
-          .catch(err => {
+                res.status(201).send(user)
+              })
+              .catch(err => {
 
-            console.log(`Error when trying to save the user "${newUser.username}"\nError: ${err.message}`.red.bold)
+                console.log(`Error when trying to save the user "${newUser.username}"\nError: ${err.message}`.red.bold)
 
-            res.status(401).send(`Error when trying to save the user \"${newUser.username}\"`)
-          })
-        }
-      })
+                res.status(401).send(`Error when trying to save the user \"${newUser.username}\"`)
+              })
+          }
+        })
     }
   },
 
   // @desc Login handler
   // @route POST /shelter/v1/auth/login
   // @access Public
-  async login (req, res) {
-    const { username, password } = req.body
+  async login(req, res) {
+    const {
+      username,
+      password
+    } = req.body
 
     // Validation
-    if(!username || !password) {
+    if (!username || !password) {
       console.log('Please, provide an username and password'.red)
       res.status(401).send({
         success: false,
@@ -106,10 +117,12 @@ export default {
     }
 
     // Search user on DB
-    const user = await User.findOne({ username }).select('+password')
+    const user = await User.findOne({
+      username
+    }).select('+password')
 
     // Check if user exists
-    if(!user) {
+    if (!user) {
       console.log(`Failed. User ${username} not found`.red)
 
       res.status(404).send({
@@ -121,7 +134,7 @@ export default {
       // Match password
       const isMatch = bcrypt.compareSync(req.body.password, user.password)
 
-      if(isMatch === true) {
+      if (isMatch === true) {
         // Required options to exchange the JWT Token
         const payload = {
           id: user._id,
@@ -131,21 +144,32 @@ export default {
 
         // Sign user in
         const token = jwt.sign(payload, process.env.JWT_SECRET, {
-          expiresIn: '1h'
+          expiresIn: '5m'
         })
 
         // Refresh Token
-        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH)
-        tokenList.push(refreshToken)
+        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH, {
+          expiresIn: 86400
+        })
 
+        // Data to send to 'tokenList'
+        const data = {
+          user: user.username,
+          role: user.role,
+          token: token,
+          refreshToken: refreshToken
+        }
+
+        // Insert the data to the Object 'tokenList'
+        tokenList[refreshToken] = data
         // console.log(tokenList)
 
         console.log(`User "${user.username}" logged in`.green)
         res.status(200).send({
           sucess: true,
           message: `User ${user.username} logged in!`,
-          role: user.role,
           user: user.username,
+          role: user.role,
           token: token,
           refreshToken: refreshToken
         })
@@ -161,53 +185,58 @@ export default {
     }
   },
   // @desc Refresh Token Handler
-  // @route GET /shelter/v1/auth/token
+  // @route POST /shelter/v1/auth/token
   // @access Private
-  async token (req, res) {
-    const newToken = req.body.refreshToken
+  async token(req, res) {
+    const refToken = req.body.refreshToken
+    const user = req.body.username
 
-    if(newToken == null || tokenList.includes(newToken)) {
-      res.status(401).send('Session expired or invalid token')
+    try {
+      if ((refToken) && (refToken in tokenList)) {
+
+        // Decoding Refresh Token to get the user
+        const userDecoded = jwt.verify(refToken, process.env.JWT_REFRESH)
+
+        const payload = {
+          id: userDecoded.id,
+          username: userDecoded.username,
+          role: userDecoded.role
+        }
+
+        console.log(payload)
+
+        // Sign in again
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+          expiresIn: '1h'
+        })
+
+        // Update Token List
+        tokenList[refToken].token = token
+
+        // console.log(tokenList)
+
+        console.log(`Token refreshed for user ${user}`.green)
+        res.status(200)
+          // .send({token: token})
+          .send({
+            sucess: true,
+            message: `Token refreshed for user ${user.username}`,
+            user: user.username,
+            token: token
+          })
+      }
+
+    } catch (err) {
+      console.log(`Error on refresh token: ${err.message}`.red)
     }
-
-    jwt.verify
-
-    // Find user on DB
-    const user = await User.findOne({ username })
-
-    const payload = {
-      id: user._id,
-      username: user.username,
-      role: user.role
-    }
-
-    // Sign in again
-    const token = jwt.sign(payload, process.env.JWT_REFRESH, {
-      expiresIn: '1h'
-    })
-
-    // Update Token List
-    tokenList.push(token)
-
-
-    console.log(`Token refreshed for user ${user.username}`.green)
-    res.status(200).send({token: token})
-    // .send({
-    //   sucess: true,
-    //   message: `Token refreshed for user ${user.username}`,
-    //   role: user.role,
-    //   user: user.username,
-    //   token: token
-    // })
-
   },
 
   // @desc Logout Handler
   // @route GET /shelter/v1/auth/logout
   // @access Private
-  logout (req, res) {
+  logout(req, res) {
 
-    if(req.headers['x-access-token']) {
+    if (req.headers['x-access-token']) {
 
       console.log('User logged out'.red)
 
@@ -231,7 +260,7 @@ export default {
   // @desc Get User Info
   // @route GET /shelter/v1/auth/user
   // @access Private
-  async user (req, res) {
+  async user(req, res) {
     let token
 
     try {
@@ -256,7 +285,7 @@ export default {
 
       // console.log(`User "${loggedUser.username}" found`.cyan)
 
-    } catch(err) {
+    } catch (err) {
 
       console.log(`Error on getting user: ${err.message}`.red)
 
